676fe8eb4d503e7789be395bbe5612c9d45ef9a6
Emilien Testu <emilientestu.pro@gmail.com>
2021-10-05 21:26:19 +0200

    Build offer & booking controllers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+    def new
+        @offer = Offer.find(params[:offer_id])
+        @user = User.find(params[:user_id])
+        @booking = Booking.new
+      end
+    
+      def create
+        @booking = Booking.new(booking_params)
+        @offer = Offer.find(params[:offer_id])
+        @user = User.find(params[:user_id])
+        @booking.list = @list
+        @booking.user = @user
+        if @booking.save
+          redirect_to list_path(@offer)
+        else
+          render :new
+        end
+      end
+    
+      def destroy
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        redirect_to list_path(@booking.offer)
+      end
+
+      private
+    
+      def booking_params
+        params.require(:booking).permit(:date, :price, :from_hour, :to_hour)
+      end
+    
+end
