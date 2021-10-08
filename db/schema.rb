049cd83b123ce33cd8b452cf90640c6686fd38aa
Adeline Cortijos <adeline.cortijos@gmail.com>
2021-10-08 10:51:13 +0200

    maj ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_173843) do
+ActiveRecord::Schema.define(version: 2021_10_07_193934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_10_07_173843) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "small_description"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
