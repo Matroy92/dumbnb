5fbf5ebbdb26a28e57655a92169d977119b6811f
Emilien Testu <emilientestu.pro@gmail.com>
2021-10-09 12:29:10 +0200

    Build create feature

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,7 +2,7 @@ class OffersController < ApplicationController
     def index
         @offer = Offer.all
         @offer = Offer.where.not(latitude: nil, longitude: nil)
-        @markers = @offers.map do |offer|
+        @markers = @offer.map do |offer|
           { lat: offer.latitude, lng: offer.longitude}
         end
     end
@@ -17,8 +17,13 @@ class OffersController < ApplicationController
     
       def create
         @offer = Offer.new(offer_params)
-        @offer.save
-        redirect_to task_path(@offer)
+        @user = current_user
+        @offer.user = @user
+        if @offer.save
+          redirect_to offers_path
+        else
+          render :new
+        end
       end
     
       def edit
@@ -41,6 +46,6 @@ class OffersController < ApplicationController
     
       def offer_params
         params.require(:offer).permit(:description, :hour_rate, :availability_start_date, :availability_end_date,
-        :availability_start_hour, :availability_end_hour, :country, :city, :radius)
+        :availability_start_hour, :availability_end_hour, :country, :city, :radius, :address, :small_description)
       end
 end
