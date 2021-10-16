6fb7aa07c29ff1e9a871f55050fffd48d9eb1ff4
Emilien Testu <emilientestu.pro@gmail.com>
2021-10-16 14:41:22 +0200

    Change style + dependence

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   geocoded_by :address
   belongs_to :user
-  has_many :booking
+  has_many :bookings, dependent: :destroy
   validates :description, :hour_rate, presence: true
 
   after_validation :geocode, if: :will_save_change_to_address?
