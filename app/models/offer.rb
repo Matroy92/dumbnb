a07ea6f8f074dcccb86cc3845f9e95bcca716bfd
Zakaria LAMJOUN <lamjounz@gmail.com>
2021-10-16 12:11:55 +0200

    tags added

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,9 +1,13 @@
 class Offer < ApplicationRecord
   geocoded_by :address
   belongs_to :user
+  
 
   validates :description, :hour_rate, presence: true
 
   after_validation :geocode, if: :will_save_change_to_address?
   has_many_attached :photos
+  acts_as_taggable_on :tags
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers do
+   # collection do
+    #  get :tag
+   # end
     resources :bookings, only: [ :show, :new, :create, :destroy ]
   end
+  get 'tags/:tag', to: 'offers#index', as: :tag
 end
